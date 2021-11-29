e277cfc1ca0a2943f8f8b30c72e021dca2aa1ea1
leoita <leonardolorusso83@gmail.com>
2021-11-29 16:27:13 +0100

    application without security

--- src/main/java/com/leoita/springsecuritybasics/SpringsecuritybasicsApplication.java
+++ src/main/java/com/leoita/springsecuritybasics/SpringsecuritybasicsApplication.java
@@ -2,8 +2,10 @@ package com.leoita.springsecuritybasics;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.leoita.controller")
 public class SpringsecuritybasicsApplication {
 
 	public static void main(String[] args) {
