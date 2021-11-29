1efcbb2b1d7aa3401417dab73277eb0431f23832
leoita <leonardolorusso83@gmail.com>
2021-11-29 21:59:48 +0100

    bank application

--- src/main/java/com/leoita/springsecurity/BankApplication.java
+++ src/main/java/com/leoita/springsecurity/BankApplication.java
@@ -1,4 +1,4 @@
-package com.leoita.springsecuritybasics;
+package com.leoita.springsecurity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan("com.leoita.controller")
-public class SpringsecuritybasicsApplication {
+public class BankApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringsecuritybasicsApplication.class, args);
+		SpringApplication.run(BankApplication.class, args);
 	}
 
 }
