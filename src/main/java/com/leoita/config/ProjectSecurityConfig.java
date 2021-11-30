2c54b310f48573c964a52a4b25cf27d0f9d34d04
leoita <leonardolorusso83@gmail.com>
2021-11-30 09:00:12 +0100

    InMemoryUserDetailsManager

--- src/main/java/com/leoita/config/ProjectSecurityConfig.java
+++ src/main/java/com/leoita/config/ProjectSecurityConfig.java
@@ -1,10 +1,15 @@
 package com.leoita.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -32,15 +37,29 @@ public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
 
     }
 
-    @Override
+/*    @Override
     protected void configure (AuthenticationManagerBuilder auth) throws Exception {
     auth.inMemoryAuthentication()
             .withUser("admin").password("12345").authorities("admin").and()
             .withUser("user").password("12345").authorities("read").and()
             .passwordEncoder(NoOpPasswordEncoder.getInstance());
     }
-    //will not read any more credentials from properties file
+  */
 
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        InMemoryUserDetailsManager userDetailsService = new InMemoryUserDetailsManager();
+        UserDetails user = User.withUsername("admin").password("12345").authorities("admin").build();
+        UserDetails user1 = User.withUsername("user").password("12345").authorities("read").build();
+        userDetailsService.createUser(user);
+        userDetailsService.createUser(user1);
+        auth.userDetailsService(userDetailsService);
+    }
+    /*we need to define a bean for PasswordEncoder, or we'll get the following error when we try to access the restricted pages after login
+     * java.lang.IllegalArgumentException: There is no PasswordEncoder mapped for the id "null"*/
 
-
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
 }
