7237bec99c7f61a34dfdc695a8110208841db6b5
leoita <leonardolorusso83@gmail.com>
2021-12-19 12:38:52 +0100

    enable WebSecurity debug (NOT FOR PROD)

--- src/main/java/com/leoita/application/BankApplication.java
+++ src/main/java/com/leoita/application/BankApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
 @ComponentScan("com.leoita.controller")
@@ -12,6 +13,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @ComponentScan("com.leoita.service")
 @EnableJpaRepositories("com.leoita.repository")
 @EntityScan("com.leoita.model")
+@EnableWebSecurity(debug = true)
 public class BankApplication {
 
     public static void main(String[] args) {
